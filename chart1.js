adf941d76ceab0c47917d222c2c4585f57bc17b8
guirosa30 <guilherme.campos.rosa@live.com>
2024-06-03 20:39:37 -0300

    Add files via upload

---
+++ chart1.js
@@ -0,0 +1,21 @@
+var ctx = document.getElementById('lineChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        datasets: [{
+            label: 'Média de paradas do mês: ',
+            data: [1.7, 1.8, 1.4, 1.6, 1.5, 1.5, 1.6, 1.4, 1.4, 1.3, 1, 1.6],
+            backgroundColor: [
+                'rgba(85,85,85, 1)'
+
+            ],
+            borderColor: 'rgb(41, 155, 99)',
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
